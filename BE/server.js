5d58dab552543a92e4fa256cd43e40c4437fa6fe
MarianiGiacomo <giacomo.mariani@gmail.com>
2017-03-08 17:30:44 +0200

    Server pyörii sivulla localhost:8081 ja palautta sivun index.html. Serverin käynnistäminen komentoriviltä BE kansiosta:
    node server.js
    
    Jos yritää hakea eri sivu esim. localhost:8081/blabla server palauttaa viesti "Requested URL /blabla does not exist..."

--- BE/server.js
+++ BE/server.js
@@ -1,32 +1,65 @@
-var express = require('express');
-var cors = require('cors');
-var app = express();
+var http = require("http");
 var fs = require("fs");
 
-app.use(cors());
+http.createServer(function(request, response) {
+		if(request.url === '/'){
+		fs.readFile('../FE/index.html', function(err, data){
+				if(err){
+				response.writeHead(404);
+				response.write('Not Found!');
+				}
+				else{
+				response.writeHead(200, {'Content-Type': 'text/html' })
+				response.write(data);
+				response.end();
+				}
+				});
+		}
+		else{
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write('<b>Hey there!</b><br /><br />Requested URL ' + request.url + ' does not exist..');
+		response.end();
+		}
+		}).listen(8081);
 
-app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
-})
-app.get('/:id', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       users = JSON.parse( data );
-       var user = users["user" + req.params.id] 
-       console.log( user );
-       res.end( JSON.stringify(user));
-   });
+// Console will print the message
+console.log('Server running ');
+
+
+
+
+
+/**
+  var express = require('express');
+  var cors = require('cors');
+  var app = express();
+  var fs = require("fs");
+
+  app.use(cors());
+
+  app.get('/listUsers', function (req, res) {
+  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+  console.log( data );
+  res.end( data );
+  });
+  })
+  app.get('/:id', function (req, res) {
+// First read existing users.
+fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+users = JSON.parse( data );
+var user = users["user" + req.params.id] 
+console.log( user );
+res.end( JSON.stringify(user));
+});
 })
 
 
 var server = app.listen(8081, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+var host = server.address().address
+var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+console.log("Example app listening at http://%s:%s", host, port)
 
 })
+ **/
